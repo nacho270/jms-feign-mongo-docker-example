2d779786f2b727fdc0efc32598cefb4e1997f89b
Ignacio Cicero <ignaciocicero@gmail.com>
2020-01-11 17:17:10 +0000

    testcontainers + swagger

--- producer/src/main/java/com/nacho/producer/ProduceController.java
+++ producer/src/main/java/com/nacho/producer/ProduceController.java
@@ -5,9 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @RestController
 public class ProduceController {
@@ -16,6 +20,13 @@ public class ProduceController {
 
     @Autowired
     private ProducerService producerService;
+
+    @GetMapping("/ping")
+    @Operation(summary = "Perfoms PING", //
+            responses = { @ApiResponse(description = "pong", responseCode = "200") })
+    public ResponseEntity<String> produce() {
+        return ResponseEntity.ok("pong");
+    }
 
     @PostMapping(value = "produce", consumes = { MediaType.APPLICATION_JSON_VALUE })
     public ResponseEntity<String> produce(@RequestBody final String body) {
